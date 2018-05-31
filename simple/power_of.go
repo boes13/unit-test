59450d1ac3774f659163fc8fffc7f3e908c78ce8
boes <riko.boestari@tokopedia.com>
2018-05-31 10:20:27 +0700

    simplified unit test PowerOf

--- simple/power_of.go
+++ simple/power_of.go
@@ -13,29 +13,9 @@ func PowerOf(basis float64, exponent int) float64 {
 	return retval
 }
 
-func PowerOf_2(basis float64, exponent int) float64 {
-	retval := float64(1)
-
-	negativeExponent := false
-	if exponent < 0 {
-		negativeExponent = true
-		exponent *= -1
-	}
-
-	for i := 0; i < exponent; i++ {
-		if negativeExponent {
-			retval *= (1 / basis)
-		} else {
-			retval *= basis
-		}
-	}
-
-	return retval
-}
-
-func PowerOf_3(basis float64, exponent int) (float64, error) {
+func PowerOf_2(basis float64, exponent int) (float64, error) {
 	if basis == 0 && exponent <= 0 {
-		return 0, fmt.Errorf("uncalculatable operation")
+		return 0, fmt.Errorf("undefined")
 	}
 
 	retval := float64(1)
--- simple/power_of_test.go
+++ simple/power_of_test.go
@@ -1,40 +1,41 @@
 package simple
 
-import "testing"
-
-type testCase struct {
-	basis    float64
-	exponent int
-	expected float64
-}
+import (
+	"log"
+	"testing"
+)
 
 func TestPowerOf(t *testing.T) {
-	testCases := []testCase{
-		//positive basis, positive or zero exponent
-		{2, 0, 1},
-		{2, 3, 8},
-		{2, 4, 16},
-
-		//negative basis, negative or zero exponent
-		{-2, 0, 1},
-		{-2, 3, -8},
-		{-2, 4, 16},
-
-		////positive basis, negative exponent
-		//{2, -1, 0.5},
-		//{2, -2, 0.25},
-		//
-		////negative basis, negative exponent
-		//{-2, -1, -0.5},
-		//{-2, -2, 0.25},
-
-		////zero basis
-		//{0, 2, 0},
-		//{0, 0, 1},
-		//{0, -2, 0},
+	type testCase struct {
+		basis    float64
+		exponent int
+		expected float64
 	}
 
-	for _, v := range testCases {
+	testCases := make(map[string]testCase)
+
+	testCases["case positive^zero"] = testCase{2, 0, 1}
+
+	testCases["case positive^positive"] = testCase{2, 3, 8}
+
+	testCases["case positive^negative"] = testCase{2, -2, 0.25}
+
+	testCases["case negative^zero"] = testCase{-2, 0, 1}
+
+	testCases["case negative^positive"] = testCase{-2, 3, -8}
+
+	testCases["case negative^positive2"] = testCase{-2, 4, 16}
+
+	testCases["case negative^negative"] = testCase{-2, -2, 0.25}
+
+	testCases["case negative^negative2"] = testCase{-2, -1, -0.5}
+
+	//testCases["case zero^positive"] = testCase{0, 2, 0}
+	//testCases["case zero^zero"] = testCase{0, 0, ??}
+	//testCases["case zero^negative"] = testCase{0, -2, error}
+
+	for k, v := range testCases {
+		log.Println("running test case:", k)
 		res := PowerOf(v.basis, v.exponent)
 		if res != v.expected {
 			t.Errorf("expected result of %f^%d=%f, got %f", v.basis, v.exponent, v.expected, res)
